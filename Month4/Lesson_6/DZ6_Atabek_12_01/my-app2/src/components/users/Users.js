d6bfe0f177b7b2699be5f6e94c458b36545e34f3
atabek06 <abdjdhdbdj@gmail.com>
2022-05-30 16:53:34 +0600

    dz6

--- Month4/Lesson_6/DZ6_Atabek_12_01/my-app2/src/components/users/Users.js
+++ Month4/Lesson_6/DZ6_Atabek_12_01/my-app2/src/components/users/Users.js
@@ -4,25 +4,21 @@ import classes from "./Users.module.css";
 function Users (props) {
     return(
         <table>
+                <tr className={classes.trr}>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>username</th>
+                    <th>email</th>
+                </tr>
             {
             props.propUsers.map((user, key) =>{
                 return(
-                    <div>
-                        <tr>
-                            <th>id</th>
-                            <th>name</th>
-                            <th>username</th>
-                            <th>email</th>
-                        </tr>
-                        <tr>
+                        <tr className={classes.trr}>
                              <td key={key}>{user.id}</td>
                              <td key={key}>{user.name}</td>
                              <td key={key}>{user.username}</td>
                              <td key={key}>{user.email}</td>
                         </tr>
-                    </div>
-
-
                 )
             })
         }
