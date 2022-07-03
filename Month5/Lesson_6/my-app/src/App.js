98d27d9fec8f9c6f48d2bbfcc96d0a37ee448bba
atabek06 <abdjdhdbdj@gmail.com>
2022-07-03 20:02:45 +0600

    dz6

---
+++ Month5/Lesson_6/my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MainPage from "./pages/mainPage/MainPage";
+import UsersPage from "./pages/usersPage/UsersPage";
+
+function App() {
+  return (
+    <div>
+      <MainPage/>
+        <br/><br/>
+        <UsersPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Month5/Lesson_6/my-app/src/components/usersList/UsersList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function UsersList({users, name}) {
+    return (
+        <ul>
+            {
+                users.length > 0
+                 ?
+
+                users.map((user, key)=> <li key={key}>{user}</li>)
+                :
+                <li>пользователей нет</li>
+
+                }
+
+        </ul>
+    );
+}
+
+
+export default UsersList;
